342d37d9787118f37f2783b9edcf446d89958a97
Truong Huu Duc <24803613+duchuuabc@users.noreply.github.com>
2023-10-10 15:04:05 +0700

    Add files via upload

---
+++ Tests/SoftHelperTests/SoftHelperTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import SoftHelper
+
+final class SoftHelperTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(SoftHelper().text, "Hello, World!")
+    }
+}
